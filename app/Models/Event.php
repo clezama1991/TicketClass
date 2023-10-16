1daaf052a4e58d29db1ab403827e867e36c4d244
Carlos Lezama <carlos.lezama@hallmg.com>
2023-10-16 10:27:58 -0400

    validacion del modelo

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -96,7 +96,7 @@ class Event extends MyBaseModel
      */
     public function imageCover()
     {
-        return ($this->images) ? $this->images[0]->image_path : null;
+        return (count($this->images)>0) ? $this->images[0]->image_path : null;
     }
 
     /**
