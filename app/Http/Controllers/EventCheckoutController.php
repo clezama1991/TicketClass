1d6baf1584f3d62bfc14db5940599d22811882a5
Carlos Lezama <carlos.lezama@hallmg.com>
2024-03-25 20:11:57 -0400

    ajuste

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -351,18 +351,76 @@ class EventCheckoutController extends Controller
     {
 
         $transaction_id = $request->get('id');
-        $order = Order::where('transaction_id', '=', $transaction_id)->first();
-        $order->is_completed_payment = true;
-        $order->save();
+		
+        $openpay = Openpay::getInstance('mgzijadvcenoigcrf4bo','sk_6e2174165e644e42b17d32409e44069c'); 
+		$charge = $openpay->charges->get($transaction_id); 
+		
+		$order = Order::where('transaction_id', '=', $transaction_id)->first();
+		
+		if($charge->status=='completed'){
+			
+			$order->is_completed_payment = true;
+			$order->save();
 
-        $url = route('showOrderDetails', [
-            'is_embedded'     => $this->is_embedded,
-            'order_reference' => $order->order_reference,
-        ]);
+			$url = route('showOrderDetails', [
+				'is_embedded'     => $this->is_embedded,
+				'order_reference' => $order->order_reference,
+			]);
 
-        return redirect($url);
+		}else{
+		
+			$attendees = Attendee::where('order_id','=',$order->id)->get();
 
+			foreach ($attendees ?? [] as $value) {
+
+				$attendee_id = $value->id;
+				$attendee = Attendee::findOrFail($attendee_id);
+				$error_message = false;
+
+
+				$attendee->ticket->decrement('quantity_sold');
+				$attendee->ticket->decrement('sales_volume', $attendee->ticket->price);
+				$attendee->ticket->decrement('organiser_fees_volume', $attendee->ticket->price_service);
+				$attendee->ticket->event->decrement('sales_volume', $attendee->ticket->price);
+				$attendee->ticket->event->decrement('organiser_fees_volume', $attendee->ticket->price_service);
+				$attendee->is_cancelled = 1;
+				$attendee->save();
+
+				$eventStats = EventStats::where('event_id', $attendee->event_id)->where('date', $attendee->created_at->format('Y-m-d'))->first();
+				if($eventStats){
+					$eventStats->decrement('tickets_sold',  1);
+					$eventStats->decrement('sales_volume',  $attendee->ticket->price);
+					$eventStats->decrement('organiser_fees_volume',  $attendee->ticket->price_service);
+				}
+
+				$order->order_status_id = 4;
+				$order->is_cancelled = true;
+				$order->save();
+				$order->delete();
+
+				if(!is_null($order->seating)){
+					$array=explode(',',$order->seating); 
+					$seatings = SeatTicket::whereIn('id',$array)->get();
+					foreach ($seatings as $key => $seating) {
+						$seating->is_available = 1;
+						$seating->save();
+					}
+				}
+			}
+			
+			session()->flash('message', 'El Pago no fue completado satisfactoriamente');
+			
+			$url = route('showEventCheckout', [
+				'event_id'     => $order->event_id,
+				'is_payment_failed' => 1,
+			]);
+		}
+		
+		return redirect($url);
     }
+
+
+
     public function postCancelOrder(Request $request)
     {
         
